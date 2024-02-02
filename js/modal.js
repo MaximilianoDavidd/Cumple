67109cfc0918bf2e49e32ffb3b889f3f254cb187
Maximiliano  Acosta <maximilianodavidacosta@hotmail.com>
2024-02-01 21:48:44 -0300

    prueba del video

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,27 @@
-// funcion para mostral el modal despues de 4 segundos
-function mostrarModal(){
-    let modal = document.querySelector('.padre');
-    modal.style.display = 'block'; // muestra el modal
-}
- // llama a la funcion despues de 4 segundos
- setTimeout(mostrarModal, 4000);
+        // Inicializar el temporizador
+        let temporizador;
 
+        function iniciarTemporizador() {
+            // Reiniciar el temporizador si ya estaba en marcha
+            if (temporizador) {
+                clearTimeout(temporizador);
+            }
+
+            // Establecer un temporizador para mostrar el modal después de 4 segundos
+            temporizador = setTimeout(mostrarModal, 4000);
+        }
+
+        // Función para mostrar el modal
+        function mostrarModal() {
+            let modal = document.querySelector('.padre');
+            modal.style.display = 'block'; // Muestra el modal
+        }
+
+        // Otras funciones del código...
+        function enviarNombre() {
+            // ...
+        }
+
+        function replayVideo() {
+            // ...
+        }
